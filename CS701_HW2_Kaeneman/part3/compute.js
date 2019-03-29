f678613db59ed7ff6867a6d3ba1d5847c43e8b0b
scott <skaeneman@gmail.com>
2019-03-28 21:39:38 -0400

    HW2-part3 save to localStorage in web browser working

--- CS701_HW2_Kaeneman/part3/compute.js
+++ CS701_HW2_Kaeneman/part3/compute.js
@@ -2,16 +2,11 @@ window.onload = init;
 
 // the worker
 var myWorker;
-// var numOfWorkers;
+var saveToLocalStorage = [];
 
 function init() {
 	var startButton = document.getElementById("startButton");
     startButton.onclick = startWorker;
-
-	// var sendButton = document.getElementById("sendButton");
-	// sendButton.onclick = sendMessageToWorker;
-	// var stopButton = document.getElementById("stopButton");
-	// stopButton.onclick = stopWorker;
 }
 
 // start the Web Worker and register the event handler
@@ -19,10 +14,6 @@ function startWorker(e) {
     // get input value
     var numOfWorkers = document.getElementById("numWorkers").value;
     var range = document.getElementById("range").value;
-
-    console.log('numOfWorkers', numOfWorkers);
-    console.log('range', range);
-
     var numOfWorkers = parseInt(numOfWorkers);
     var range = parseInt(range);
 
@@ -33,51 +24,43 @@ function startWorker(e) {
     } else {
         // else an integer was received so kick off the web worker
         if (myWorker == null) {
+            // split up the range by the number of workers
             var numMax = range / numOfWorkers;
-            var numLow = 1;
-            var numHigh = numMax;
+            var numLow = 1;  // starting number
+            var numHigh = numMax;  // ending number
 
             // loop to create each web worker
             for (var i = 0; i < numOfWorkers; i++) {
-
                 myWorker = new Worker("computeWorker.js");
                 myWorker.addEventListener("message", handleReceipt, false);
                 
-                console.log('inside startWorker...');
-                console.log('numLow', numLow);
-                console.log('numHigh', numHigh);
-
                 sendMessageToWorker(e, numLow, numHigh, i);
+                // increment to create the next block of numbers 
                 numLow += numMax;
                 numHigh += numMax;
             }
-
         }	
     }
 }
 
 // Handle messages received from the Web Worker
 function handleReceipt(event) {
-    console.log('inside handleReceipt...');
-    console.log(event.data.start);
 
-    var output = JSON.stringify(event.data);
+    saveToLocalStorage.push(event.data);
+    console.log('saveToLocalStorage', saveToLocalStorage)
+    localStorage.setItem('HW2-Part3', JSON.stringify(saveToLocalStorage));    
+
 
     var itemsList = document.getElementById("items");
     var item = document.createElement("li");
-    item.innerHTML = output;
+    item.innerHTML = JSON.stringify(event.data);
     items.appendChild(item);
 }
 
 // send message to the Web Worker
 function sendMessageToWorker(e, numLow, numHigh, count) {
-
-    console.log('inside sendMessageToWorker...');
-    console.log('numLow', numLow);
-    console.log('numHigh', numHigh);
-
     // JSON object to pass high and low numbers to worker
-    numberBlock = {numStart: numLow, numEnd: numHigh, workerNum: count};
+    numberBlock = {index: count, numStart: numLow, numEnd: numHigh};
 
     if (myWorker != null) {
         myWorker.postMessage(numberBlock);
--- CS701_HW2_Kaeneman/part3/computeWorker.js
+++ CS701_HW2_Kaeneman/part3/computeWorker.js
@@ -2,22 +2,20 @@ self.onmessage = messageHandler;
 // or,
 //addEventListener("message", messageHandler, true);
 
-// var counter = 1;
-
 function messageHandler(e) {
-
-  // computes sum of all integers from the start value to the end value
   var numStart = parseInt(e.data.numStart);
   var numEnd = parseInt(e.data.numEnd);
-  var workerNum = parseInt(e.data.workerNum);
+  var index = parseInt(e.data.index);
+
+  console.log('worker received:', JSON.stringify(e.data));
 
   var sum = getSum(numStart, numEnd);
-  var result = {index: workerNum, start: numStart, end: numEnd, result: sum};
+  var result = {index: index, start: numStart, end: numEnd, result: sum};
   // result is sent as a JSON object having start, end, and result properties
   postMessage(result);
 }
 
-// adds every integer together from numStart to numEnd
+// computes sum of all integers from the start value to the end value
 function getSum(numStart, numEnd) {
   var sum = 0;
   for (var i=numStart; i <= numEnd; i++) {
