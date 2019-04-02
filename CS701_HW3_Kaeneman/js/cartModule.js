dc4650aa203b94fe600011dd0d3be35c5ce8e2e9
scott <skaeneman@gmail.com>
2019-04-02 00:03:20 -0400

    HW3-part1 Add to localStorage in browser

--- CS701_HW3_Kaeneman/js/cartModule.js
+++ CS701_HW3_Kaeneman/js/cartModule.js
@@ -1,17 +1,25 @@
 var cartModule = angular.module('cart', [])
 
 cartModule.controller('CartController', function ($scope) {
-    $scope.books = [
-      {title: 'Absolute Java',    
-          qty: 1, price: 114.95},
-      {title: 'Pro HTML5',        
-          qty: 1, price: 27.95},
-      {title: 'Head First HTML5', 
-          qty: 1, price: 27.89}
-    ];
+
+    // check localStorage to see if books alredy exist
+    var localStorageBooks = JSON.parse(localStorage.getItem('kaeneman_cart'));
+    if (localStorageBooks) {
+        $scope.books = localStorageBooks;
+    } else {
+        $scope.books = [
+        {title: 'Absolute Java',    
+            qty: 1, price: 114.95},
+        {title: 'Pro HTML5',        
+            qty: 1, price: 27.95},
+        {title: 'Head First HTML5', 
+            qty: 1, price: 27.89}
+        ];
+    }
 
     $scope.removeBook = function(index) {
       $scope.books.splice(index, 1);
+    //   $scope.saveBook(); // update localStorage
     }
 
     // add a new book
@@ -22,7 +30,12 @@ cartModule.controller('CartController', function ($scope) {
          price: 10.99
       })
     }
-    
+
+    // save a book to localStorage
+    $scope.saveBook = function(index) {
+        localStorage.setItem('kaeneman_cart', angular.toJson($scope.books));
+    }
+
     // update book total
     var books = $scope.books;
     $scope.updateTotal = function() {
