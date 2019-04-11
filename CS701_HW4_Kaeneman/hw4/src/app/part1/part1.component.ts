40e6188604e02adf25dd2de34bdccccdd25cad10
scott <skaeneman@gmail.com>
2019-04-10 23:45:24 -0400

    HW4-part1 add rest of functions.  project is complete.

--- CS701_HW4_Kaeneman/hw4/src/app/part1/part1.component.ts
+++ CS701_HW4_Kaeneman/hw4/src/app/part1/part1.component.ts
@@ -29,4 +29,33 @@ export class Part1Component implements OnInit {
     }    
   }
 
+  // delete a book
+  removeBook = function(index) {
+    this.books.splice(index, 1);
+  }  
+
+    // add a new book
+    newBook = function() {
+      this.books.push({
+         title: 'New Book',    
+         qty: 1, 
+         price: 10.99
+      })
+    }  
+
+    // store book in localStorage
+    saveBook = function() {
+      localStorage.setItem('kaeneman_cart', JSON.stringify(this.books));
+  }    
+
+    // update book total    
+    updateTotal = function() {
+      let total = 0;
+      let bks = this.books;
+      for (var i = 0; i < bks.length; i++) { 
+        total += (bks[i].price * bks[i].qty);
+      }
+      return total;
+  }  
+
 }
